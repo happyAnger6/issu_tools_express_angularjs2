84ef8ec7454adbf96fd5c0f9fa37b1c7b5ac3597
happyAner6 <happyAnger6@163.com>
2018-01-27 20:04:18 +0800

    add parent branch

--- routes/api/branchs.js
+++ routes/api/branchs.js
@@ -16,12 +16,28 @@ router.get('/json', function(req, res, next) {
 router.post('/create', function(req, res, next){
     console.log(req.body);
     var branch = new Branch(req.body);
-    branch.save(function (err, u){
-        if (err) {
-            return next(err);
-        }
-        res.json(u);
-    })
+    if (branch.ParentName) {
+       Branch.findOne({Name: branch.ParentName}, function(err, parent){
+           if (err) return next(err);
+           parent.ChildrenNames.push(branch.Name);
+           parent.save(function (err, parent){
+               if (err) return next(err);
+               branch.save(function (err, u){
+                   if (err) {
+                       return next(err);
+                   }
+                   res.json(u);
+               })
+           })
+       })
+    } else {
+        branch.save(function (err, u) {
+            if (err) {
+                return next(err);
+            }
+            res.json(u);
+        })
+    }
 });
 
 router.post('/:branch/delete', function(req, res, next){
